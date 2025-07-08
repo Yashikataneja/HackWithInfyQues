966528b3cc1b2ba11b6fa355c5476093d9604fc4
YashikaTaneja <yashika979.be22@chitkara.edu.in>
2025-07-08 10:27:06 +0530

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FirstProgram
+{
+public static void main(String[] args)
+{
+	
+System.out.println("hello");
+Scanner sc=new Scanner(System.in);
+
+
+
+}
+
+}
